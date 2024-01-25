9291b4b55cbeac1fe9a1810bb2ada529ac0246f6
yonatan afewerk <yonatanafewerk@gmail.com>
2024-01-25 20:51:29 +0300

    Problem: using numpy and cv2 to edit the images before feeding them to tesseract

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 import PIL.Image
 import pytesseract
+import os
+import cv2
+import numpy as np
 
 
-text = pytesseract.image_to_string(PIL.Image.open("./assets/photo.jpg"))
+def get_files_in_folder(path):
+    if not os.path.exists(path):
+        return []
+    files = os.listdir(path)
+    return files
 
-print(text)
+
+def opening(image):
+    kernel = np.ones((5, 5), np.uint8)
+    return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
+
+
+path = "./assets"
+file_names = get_files_in_folder(path)
+
+my_config = r"--psm 6 --oem 3"
+content = []
+
+for file in file_names:
+    image = cv2.imread(f"./assets/{file}")
+    edited_image = opening(image)
+    text = pytesseract.image_to_string(edited_image, config=my_config)
+    content.append(text)
+
+for read in content:
+    print(read)
