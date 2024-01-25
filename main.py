6e57d343b21cfe6b7fe8e71214062c869199e7ae
yonatan afewerk <yonatanafewerk@gmail.com>
2024-01-25 20:59:52 +0300

    Problem: running it through a bunch of different img processing but still some errors

--- main.py
+++ main.py
@@ -12,9 +12,50 @@ def get_files_in_folder(path):
     return files
 
 
+def get_grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+
+def remove_noise(image):
+    return cv2.medianBlur(image, 5)
+
+
+def thresholding(image):
+    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+
+def dilate(image):
+    kernel = np.ones((5, 5), np.uint8)
+    return cv2.dilate(image, kernel, iterations=1)
+
+
+def erode(image):
+    kernel = np.ones((5, 5), np.uint8)
+    return cv2.erode(image, kernel, iterations=1)
+
+
 def opening(image):
     kernel = np.ones((5, 5), np.uint8)
     return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
+
+
+def canny(image):
+    return cv2.Canny(image, 100, 200)
+
+
+def deskew(image):
+    coords = np.column_stack(np.where(image > 0))
+    angle = cv2.minAreaRect(coords)[-1]
+    if angle < -45:
+        angle = -(90 + angle)
+    else:
+        angle = -angle
+    (h, w) = image.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotated = cv2.warpAffine(
+        image, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+    return rotated
 
 
 path = "./assets"
@@ -25,8 +66,13 @@ content = []
 
 for file in file_names:
     image = cv2.imread(f"./assets/{file}")
-    edited_image = opening(image)
-    text = pytesseract.image_to_string(edited_image, config=my_config)
+
+    gray = get_grayscale(image)
+    thresh = thresholding(gray)
+    o = opening(gray)
+    c = canny(gray)
+
+    text = pytesseract.image_to_string(c, config=my_config)
     content.append(text)
 
 for read in content:
